717f60aaa2bb887ecae995fa400006c14836a980
Jake <dawkinsjh@gmail.com>
2019-06-11 14:25:55 -0400

    Fixed enum validation falsely failing

--- packages/apollo-federation/src/composition/validate/preComposition/__tests__/duplicateEnumOrScalar.test.ts
+++ packages/apollo-federation/src/composition/validate/preComposition/__tests__/duplicateEnumOrScalar.test.ts
@@ -5,7 +5,34 @@ import { graphqlErrorSerializer } from '../../../../snapshotSerializers';
 expect.addSnapshotSerializer(graphqlErrorSerializer);
 
 describe('duplicateEnumOrScalar', () => {
-  it('', () => {
+  it('does not error with proper enum and scalar usage', () => {
+    const serviceA = {
+      typeDefs: gql`
+        type Product @key(fields: "color { id value }") {
+          sku: String!
+          upc: String!
+          shippingDate: Date
+          type: ProductType
+        }
+
+        enum ProductType {
+          BOOK
+          FURNITURE
+        }
+
+        extend enum ProductType {
+          DIGITAL
+        }
+
+        scalar Date
+      `,
+      name: 'serviceA',
+    };
+
+    const warnings = validateDuplicateEnumOrScalar(serviceA);
+    expect(warnings).toEqual([]);
+  });
+  it('errors when there are multiple definitions of the same enum', () => {
     const serviceA = {
       typeDefs: gql`
         type Product @key(fields: "color { id value }") {
@@ -32,6 +59,39 @@ describe('duplicateEnumOrScalar', () => {
     };
 
     const warnings = validateDuplicateEnumOrScalar(serviceA);
-    expect(warnings).toEqual([]);
+    expect(warnings).toMatchInlineSnapshot(`
+            Array [
+              Object {
+                "code": "DUPLICATE_ENUM_DEFINITION",
+                "message": "[serviceA] ProductType -> The enum, \`ProductType\` was defined multiple times in this service. Remove one of the definitions for \`ProductType\`",
+              },
+            ]
+        `);
+  });
+
+  it('errors when there are multiple definitions of the same scalar', () => {
+    const serviceA = {
+      typeDefs: gql`
+        scalar Date
+        type Product @key(fields: "color { id value }") {
+          sku: String!
+          upc: String!
+          deliveryDate: Date
+        }
+
+        scalar Date
+      `,
+      name: 'serviceA',
+    };
+
+    const warnings = validateDuplicateEnumOrScalar(serviceA);
+    expect(warnings).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "code": "DUPLICATE_SCALAR_DEFINITION",
+          "message": "[serviceA] Date -> The scalar, \`Date\` was defined multiple times in this service. Remove one of the definitions for \`Date\`",
+        },
+      ]
+    `);
   });
 });
